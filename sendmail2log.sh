c615cbf14da356a035609bb3c4a8358abefcab70
gondidi <nakagawa@n-sen.com>
2019-06-30 16:18:37 +0900

    modified sendmail 	modified:   sendmail2log.sh

--- sendmail2log.sh
+++ sendmail2log.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # sudo ln -s /var/www/html/rrs/sendmail2log.sh /usr/sbin/sendmail
+# sudo ln -sf /usr/bin/vim /etc/alternatives/editor
+# sudo /etc/init.d/rsyslog restart
 echo "$0 $@" | logger -p cron.err 
 logger -p cron.info
-# sudo /etc/init.d/rsyslog restart
-# sudo ln -sf /usr/bin/vim /etc/alternatives/editor
