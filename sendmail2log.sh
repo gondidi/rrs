62380d66f51fd85598175e257170bb21fab41c62
gondidi <nakagawa@n-sen.com>
2019-07-06 19:49:25 +0900

    installation of crontab

--- sendmail2log.sh
+++ sendmail2log.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 # sudo ln -s /var/www/html/rrs/sendmail2log.sh /usr/sbin/sendmail
-# sudo ln -sf /usr/bin/vim /etc/alternatives/editor
-# sudo /etc/init.d/rsyslog restart
 echo "$0 $@" | logger -p cron.err 
 logger -p cron.info
